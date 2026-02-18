5d001106bfcb0414c9023d368cf23497685abede
engsr6982 <pwxg286704@163.com>
2026-02-18 14:42:32 +0800

    feat(binding): add TypeTraits and FunctionTraits for type handling

---
+++ src/v8kit/binding/traits/TypeTraits.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <type_traits>
+
+#include <string_view>
+
+#include "v8kit/core/Concepts.h"
+
+namespace v8kit::binding::traits {
+
+template <typename T>
+struct RawTypeHelper {
+    using Decayed = std::decay_t<T>;
+
+    // static_assert(
+    //     !std::is_pointer_v<Decayed> || concepts::StringLike<Decayed>,
+    //     "RawType does not allow raw pointer types except string-like"
+    // );
+
+    using type = std::conditional_t<
+        concepts::StringLike<Decayed>,
+        std::string_view,              // 命中 StringLike -> 强制映射为 string_view (fix const char[N])
+        std::remove_pointer_t<Decayed> // 其他类型 -> 移除指针
+        >;
+};
+
+template <typename T>
+using RawType_t = typename RawTypeHelper<T>::type;
+
+template <typename T>
+inline constexpr size_t size_of_v = sizeof(T);
+
+template <>
+inline constexpr size_t size_of_v<void> = 0;
+
+
+} // namespace v8kit::binding::traits
