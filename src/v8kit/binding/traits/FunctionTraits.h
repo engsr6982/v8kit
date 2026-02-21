0cfd2b557b5b9500e73a862b0e866c4f148d31fe
engsr6982 <pwxg286704@163.com>
2026-02-21 22:33:43 +0800

    refactor: Add isConst trait to FunctionTraits

--- src/v8kit/binding/traits/FunctionTraits.h
+++ src/v8kit/binding/traits/FunctionTraits.h
@@ -15,6 +15,7 @@ struct FunctionTraits<R (*)(Args...)> {
     using ReturnType                  = R;
     using ArgsTuple                   = std::tuple<Args...>;
     static constexpr size_t ArgsCount = sizeof...(Args);
+    static constexpr bool   isConst   = false; // always false
 };
 
 template <typename R, typename... Args>
@@ -24,16 +25,23 @@ struct FunctionTraits<R(Args...)> : FunctionTraits<R (*)(Args...)> {};
 template <typename R, typename... Args>
 struct FunctionTraits<std::function<R(Args...)>> : FunctionTraits<R (*)(Args...)> {};
 
+
 // 成员函数指针（包括 const / noexcept 等）
 template <typename C, typename R, typename... Args>
 struct FunctionTraits<R (C::*)(Args...)> {
     using ReturnType                  = R;
     using ArgsTuple                   = std::tuple<Args...>;
     static constexpr size_t ArgsCount = sizeof...(Args);
+    static constexpr bool   isConst   = false; // always false
 };
 
 template <typename C, typename R, typename... Args>
-struct FunctionTraits<R (C::*)(Args...) const> : FunctionTraits<R (C::*)(Args...)> {};
+struct FunctionTraits<R (C::*)(Args...) const> {
+    using ReturnType                  = R;
+    using ArgsTuple                   = std::tuple<Args...>;
+    static constexpr size_t ArgsCount = sizeof...(Args);
+    static constexpr bool   isConst   = true; // const modifier
+};
 
 template <typename C, typename R, typename... Args>
 struct FunctionTraits<R (C::*)(Args...) const noexcept> : FunctionTraits<R (C::*)(Args...) const> {};
